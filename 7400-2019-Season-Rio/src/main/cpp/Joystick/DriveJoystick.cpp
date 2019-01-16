d73f5ab3c311ee80cbff5a9415cd9f56bf00c58e
Andrew Menkes <amenkes319@gmail.com>
2019-01-15 21:57:25 -0500

    Added joystick modifications
    prints out mapped functions in console

--- 7400-2019-Season-Rio/src/main/cpp/Joystick/DriveJoystick.cpp
+++ 7400-2019-Season-Rio/src/main/cpp/Joystick/DriveJoystick.cpp
@@ -2,22 +2,16 @@
 
 DriveJoystick::DriveJoystick(int joyStickPort)
 			 : MeeseeksJoystick(joyStickPort),
-			   m_grab(0, 2, 0, true, 1),
-			   m_frontLeftCenter(4), m_frontRightCenter(5), m_backLeftCenter(2), m_backRightCenter(3), m_centerCenter(1),
-			   m_disableSwerve(10, 2, 0, true, 1), m_writeSteerOffsets(11), m_zeroSwerve(9), m_onlyX(6), m_onlyY(7), m_onlyZ(8)
+			   m_allign(0), m_eject(1), m_elevatorFlip(2), m_intake(3),
+			   m_topHeight(6), m_midHeight(8), m_bottomHeight(10)
 {
-	m_buttons.push_back(&m_grab);
-	m_buttons.push_back(&m_frontLeftCenter);
-	m_buttons.push_back(&m_frontRightCenter);
-	m_buttons.push_back(&m_backLeftCenter);
-	m_buttons.push_back(&m_backRightCenter);
-	m_buttons.push_back(&m_centerCenter);
-	m_buttons.push_back(&m_disableSwerve);
-	m_buttons.push_back(&m_writeSteerOffsets);
-	m_buttons.push_back(&m_onlyX);
-	m_buttons.push_back(&m_onlyY);
-	m_buttons.push_back(&m_onlyZ);
-	m_buttons.push_back(&m_zeroSwerve);
+	m_buttons.push_back(&m_allign);
+	m_buttons.push_back(&m_eject);
+	m_buttons.push_back(&m_elevatorFlip);
+	m_buttons.push_back(&m_intake);
+	m_buttons.push_back(&m_topHeight);
+	m_buttons.push_back(&m_midHeight);
+	m_buttons.push_back(&m_bottomHeight);
 }
 
 void DriveJoystick::Periodic()
--- 7400-2019-Season-Rio/src/main/cpp/Joystick/DriveJoystick.h
+++ 7400-2019-Season-Rio/src/main/cpp/Joystick/DriveJoystick.h
@@ -7,26 +7,19 @@ class DriveJoystick : public MeeseeksJoystick
 {
 	public    : DriveJoystick(int joyStickPort);
 
-				JoystickValueButton  *Grab()               { return &m_grab;              }
-				JSButton             *FrontLeftCenter()    { return &m_frontLeftCenter;   }
-				JSButton             *FrontRightCenter()   { return &m_frontRightCenter;  }
-				JSButton             *BackLeftCenter()     { return &m_backLeftCenter;    }
-				JSButton             *BackRightCenter()    { return &m_backRightCenter;   }
-				JSButton             *CenterCenter()       { return &m_centerCenter;      }
-				JoystickValueButton  *DisableSwerve()      { return &m_disableSwerve;     }
-				JSButton  			 *WriteSwerveOffsets() { return &m_writeSteerOffsets; }
-				JSButton			 *OnlyX()			   { return &m_onlyX;			  }
-				JSButton			 *OnlyY()			   { return &m_onlyY;			  }
-				JSButton			 *OnlyZ()			   { return &m_onlyZ;			  }
-				JSButton			 *ZeroSwerve()		   { return &m_zeroSwerve;		  }
-
+				JSButton *Allign()	   	 { return &m_allign; 	   }
+				JSButton *Eject()	   	 { return &m_eject;		   }
+				JSButton *ElevatorFlip() { return &m_elevatorFlip; }
+				JSButton *Intake()	   	 { return &m_intake;	   }
+				JSButton *TopHeight()	 { return &m_topHeight;	   }
+				JSButton *MidHeight()	 { return &m_midHeight;	   }
+				JSButton *BottomHeight() { return &m_bottomHeight; }
+				
 				void Periodic();
 	protected :
-				JoystickValueButton  m_grab;
-				JSButton             m_frontLeftCenter, m_frontRightCenter, m_backLeftCenter, m_backRightCenter, m_centerCenter;
-				JoystickValueButton  m_disableSwerve;
-				JSButton			 m_writeSteerOffsets, m_zeroSwerve;
-				JSButton			 m_onlyX, m_onlyY, m_onlyZ;
+				JSButton m_allign, m_eject, m_elevatorFlip, m_intake;
+				JSButton m_topHeight, m_midHeight, m_bottomHeight;
+				
 };
 
 #endif /* SRC_DRIVEJOYSTICK_H_ */
