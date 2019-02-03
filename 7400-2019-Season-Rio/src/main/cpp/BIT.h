eae4279274ae2fa1e07fade892e6fac277514ed6
Rob Rosloniec <robrosloniec@gmail.com>
2019-02-03 17:22:35 -0500

    Added Template for BIT

---
+++ 7400-2019-Season-Rio/src/main/cpp/BIT.h
@@ -0,0 +1,24 @@
+
+typedef enum
+{
+    eSteerTest = 0,
+    eRotationTest,
+    eIntakeMotorTest,
+    eIntakeMechanismRotation,
+    eLadderTest,
+    eFlipTest,
+    eCargoGrabberTest,
+    eHatchGrabTest,
+    eHatchLinearTest,
+
+
+}BIT_State;
+
+
+
+class BIT
+{
+    public : BIT();
+            void Run();
+
+};
