ce7f7caf1a6dc412a7f66b84c106607a9cd0403d
Andrew Menkes <amenkes319@gmail.com>
2019-04-08 19:09:55 -0400

    Hatch Comments, WIP

--- 7400-2019-Season-Rio/src/main/cpp/SubSystems/HatchControl.cpp
+++ 7400-2019-Season-Rio/src/main/cpp/SubSystems/HatchControl.cpp
@@ -11,7 +11,6 @@ HatchControl::HatchControl(int slideID, int grabberID)
 {
 		m_hatchGrabCounter = 0;
 		m_hatchGrabInitialPosition = 0;
-		m_hatchGrabInitialPosition = 0;
 }
 
 void HatchControl::Initialize()
@@ -34,7 +33,7 @@ void HatchControl::Periodic()
 
 	//printf("%d %d\n", g_rc.m_cargoSwitch.Get(), g_rc.m_gamePieceSwitch.Get());
 
-    switch(m_hatchSliderState)
+    switch(m_hatchSliderState) //to get out of the In or Out states, that is controlled by the switch(m_hatchGrabState) {}
 	{
 		case eHatchSliderStateInitialize:
 			m_hatchSlide.Set(0.4); //Moves slider in
@@ -88,29 +87,29 @@ void HatchControl::Periodic()
 			sliderError = -320 - m_hatchSlide.GetSelectedSensorPosition(); //finds error between out position and current position
 
 			//if the error  is less than 30 (close to out) current limit is 0.6 amps
-			//if error is greater than 30, current limit is constant
+			//if error is greater than 30, current limit is the constant, 1.8 amps
 			if(m_hatchSlide.GetOutputCurrent() >= (fabs(sliderError) < 30 ? 0.6 : HATCH_SLIDER_OUT_CURRENT_THRESHOLD))
 				m_currentCounter++;
 			else
 				m_currentCounter = 0;
 
-			if(fabs(sliderError) <= 10 || m_currentCounter >= HATCH_CURRENT_ITERATIONS)
+			if(fabs(sliderError) <= 10 || m_currentCounter >= HATCH_CURRENT_ITERATIONS) //check to see if the slider is close to out OR hits a current limit
 			{
 				m_hatchSlide.Set(0);
 				m_hatchSliderState = eHatchSliderStateOut;
 			}
 			else
 			{
-				if(fabs(sliderError) < 30)
-					m_hatchSlide.Set(0.3/100 * sliderError - 0.2);
-				else
-					m_hatchSlide.Set(-0.5);
+				if(fabs(sliderError) < 30) //Checks if slider is within 30 encoder ticks of 320 (getting closer to out)
+					m_hatchSlide.Set(0.3/100 * sliderError - 0.2); //Set speed to slow down as slider gets closer to 320
+				else //If slider is far from out
+					m_hatchSlide.Set(-0.5); //Sets speed to a constant
 			}
 
 			break;
 
 		case eHatchSliderStateOut:
-			m_hatchSlide.Set(0.0);
+			m_hatchSlide.Set(0.0); //waiting for grabbers
 
 			m_currentCounter = 0;
 
@@ -120,20 +119,20 @@ void HatchControl::Periodic()
 	switch(m_hatchGrabState)
 	{
 		case eHatchGrabStateInitialize:
-			m_hatchGrab.Set(-0.4);
+			m_hatchGrab.Set(-0.4); //Move grabbers out
 		
-			if(m_hatchGrab.GetOutputCurrent() >= HATCH_GRAB_CURRENT_THRESHOLD)
+			if(m_hatchGrab.GetOutputCurrent() >= HATCH_GRAB_CURRENT_THRESHOLD) //Check if grabbers hit a current limit
 			{
 				if(++m_hatchGrabCounter == HATCH_GRAB_ITERATIONS)
 				{
-					if(g_rc.m_gamePieceSwitch.Get() && !g_rc.m_cargoSwitch.Get())
+					if(g_rc.m_gamePieceSwitch.Get() && !g_rc.m_cargoSwitch.Get()) ////Pull in the hatch at the start if the game piece switch is true and the cargo switch is false
 					{
-						SetEncoderPosition(m_hatchGrab, HATCH_GRAB_HOLDING_POSITION);
+						SetEncoderPosition(m_hatchGrab, HATCH_GRAB_HOLDING_POSITION); //if there is a hatch at the start, when a current limit is hit, set the encoder position to holding
 						m_hatchGrabState = eHatchGrabStateAcquried;
 					}
 					else
 					{
-						SetEncoderPosition(m_hatchGrab, HATCH_GRAB_NOT_HOLDING_POSITION);
+						SetEncoderPosition(m_hatchGrab, HATCH_GRAB_NOT_HOLDING_POSITION); //if there is not hatch a hatch at the start, when a current limit is hit, set the encoder to not holding
 						m_hatchGrabState = eHatchGrabStateNotHolding;
 					}
 				}
@@ -142,21 +141,21 @@ void HatchControl::Periodic()
 			break;
 
 		case eHatchGrabStateAcquring:
-			grabError = m_hatchGrabInitialPosition - m_hatchGrab.GetSelectedSensorPosition();
+			grabError = m_hatchGrabInitialPosition - m_hatchGrab.GetSelectedSensorPosition(); //Find error from the initial position (value from initialize state) and current position
 			
-			if(m_hatchSliderState == eHatchSliderStateOut)
-				m_hatchGrab.Set(-0.6);
+			if(m_hatchSliderState == eHatchSliderStateOut) //check if the slider is out to move the grabbers out
+				m_hatchGrab.Set(-0.6); //grabbers go out to start acquiring the hatch
 
-			if(m_hatchGrab.GetSelectedSensorPosition() >= 310)
-				m_hatchGrabState = eHatchGrabStateNotHolding;
+			if(m_hatchGrab.GetSelectedSensorPosition() >= HATCH_GRAB_HOLDING_POSITION + 40) //if grabbers go past the grab position while trying to grab a hatch
+				m_hatchGrabState = eHatchGrabStateNotHolding; //go back to start (auto abort)
 
-			if(m_hatchGrab.GetOutputCurrent() >= HATCH_GRAB_CURRENT_THRESHOLD)
+			if(m_hatchGrab.GetOutputCurrent() >= HATCH_GRAB_CURRENT_THRESHOLD)//check if hatch grabbers hit a current limit (hit the hatch)
 			{
 				if(++m_hatchGrabCounter == HATCH_GRAB_ITERATIONS)
 				{
 					m_hatchRaiseCounter = 0;
 					m_hatchGrabState = eHatchGrabStateAcquried;
-					m_hatchSliderState = eHatchSliderStateMovingIn;
+					m_hatchSliderState = eHatchSliderStateMovingIn; //when hatch is acquired, slider moves in
 				}
 			}
 			else
@@ -165,19 +164,19 @@ void HatchControl::Periodic()
 			break;
 
     	case eHatchGrabStateAcquried:
-			m_hatchGrab.Set(-0.4);
+			m_hatchGrab.Set(-0.4); //set grab speed in order to hold the hatch (not let go)
 
-			if(++m_hatchRaiseCounter == 50)
+			if(++m_hatchRaiseCounter == 50) 
 			{
-				//g_rc.m_ladderTargetHeight = eLadderHeightReceiveHatch;
+				//g_rc.m_ladderTargetHeight = eLadderHeightReceiveHatch; //raise hatch height in order to see through camera, not used.
 			}
 
-			if(!g_rc.m_bCargo && g_rc.m_bAction || g_rc.m_bAbort)
+			if(!g_rc.m_bCargo && g_rc.m_bAction || g_rc.m_bAbort) //in hatch mode and either the action or abort button is pressed, eject the hatch
 			{
 				m_hatchSliderState = eHatchSliderStateMovingOut;
 			}
 
-			if(m_hatchSliderState == eHatchSliderStateOut)
+			if(m_hatchSliderState == eHatchSliderStateOut) //wait until slider is out from above, then start ejecting
 			{
 				m_hatchGrabState = eHatchGrabStateEjecting;
 			}
@@ -185,30 +184,30 @@ void HatchControl::Periodic()
 			break;
 		
     	case eHatchGrabStateNotHolding:
-			grabError = 0 - m_hatchGrab.GetSelectedSensorPosition();
-			m_hatchGrab.Set(0.2);
+			grabError = 0 - m_hatchGrab.GetSelectedSensorPosition(); //find error between 0 and current position
+			m_hatchGrab.Set(0.2); //move hatch grabbers in, toward 0
 
-			if(m_hatchSliderState == eHatchSliderStateOut)
+			if(m_hatchSliderState == eHatchSliderStateOut) //wait until the slider is fully out to start moving it back in
 				m_hatchSliderState = eHatchSliderStateMovingIn;
 
-			if(fabs(grabError) <= 5)
+			if(fabs(grabError) <= 5) //check if hatch grabbers are close to 0 (close to in)
 				m_hatchGrabState = eHatchGrabStateWaiting;
 		
 			break;
 		
 		case eHatchGrabStateEjecting:
-			grabError = -10 - m_hatchGrab.GetSelectedSensorPosition();
-			m_hatchGrab.Set(0.4);
+			grabError = -10 - m_hatchGrab.GetSelectedSensorPosition(); //find error between -10 (close to in) and current position
+			m_hatchGrab.Set(0.4); //move grabbers in
 
-			if(fabs(grabError) <= 10)
+			if(fabs(grabError) <= 10) //check if grabbers are close to in (ejected)
 			{
-				m_hatchSliderState = eHatchSliderStateMovingIn;
-				m_hatchGrabState = eHatchGrabStateWaiting;
+				m_hatchSliderState = eHatchSliderStateMovingIn; //once hatch ejected, start moving slider in
+				m_hatchGrabState = eHatchGrabStateWaiting; //hatch ejected, set grabbers to waiting
 			}
 		
 			break;
 		
-		case eHatchGrabStateWaiting:
+		case eHatchGrabStateWaiting: //this is the empty state, the state it is when driving around without a hatch, waiting to acquire a hatch
 			m_hatchGrab.Set(0);
 
 			if(!g_rc.m_bCargo)
